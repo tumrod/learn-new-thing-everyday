79ba3ee9c447d07b48ae50e4ce0aec8cd93277d7
tumrod <tipparat.umrod@utexas.edu>
2015-08-10 11:01:48 -0500

    javascript and jQuery fun

---
+++ random/exploded-planet/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+    $('div').click(function() {
+            $(this).fadeOut('fast');
+            $isClicked = true;
+    });
+    $('div').hover(function() {
+        $(this).addClass('red');
+    });
+
+});
